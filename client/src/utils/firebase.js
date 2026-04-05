dc72c610541459fa116e63e4fad69abcf1613d8e
AyushRawat17-code <ayushrawat12321@gmail.com>
2026-04-05 22:23:34 +0530

    Update firebase.js

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-65f4e.firebaseapp.com",  // ← fix this
+  apiKey: "AIzaSyBe0ZYYhjZgK2CmwgWs2xjd7MfAy1VDfKY",
+  authDomain: "interviewiq-65f4e.firebaseapp.com",
   projectId: "interviewiq-65f4e",
   storageBucket: "interviewiq-65f4e.firebasestorage.app",
-  messagingSenderId: "606257719601",
-  appId: "1:606257719601:web:27742342b6d10e46122810"
+  messagingSenderId: "839297223316",
+  appId: "1:839297223316:web:f4469599a0a72d28d2c281"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider()
 
-export {auth , provider}
+export { auth, provider }
