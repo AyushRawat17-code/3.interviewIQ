2146340202360a76f0cbe655599eab85ec617203
AyushRawat17-code <ayushrawat12321@gmail.com>
2026-04-05 21:53:58 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,14 +16,7 @@ export const googleAuth = async (req,res) => {
 
     let token = await genToken(user._id)
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    })
-
-    return res.status(200).json(user)
+    return res.status(200).json({...user._doc, token})
 
   } catch (error) {
     return res.status(500).json({message:`Google auth error ${error}`})
@@ -32,11 +25,6 @@ export const googleAuth = async (req,res) => {
 
 export const logOut = async (req,res) => {
   try {
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none"
-    })
     return res.status(200).json({message:"LogOut Successfully"})
   } catch (error) {
     return res.status(500).json({message:`Logout error ${error}`})
