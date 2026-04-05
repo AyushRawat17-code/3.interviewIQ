407d56620fb5c1d5abf7f2625e3f5d5328d3ae2e
AyushRawat17-code <ayushrawat12321@gmail.com>
2026-04-05 21:18:55 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,41 +1,44 @@
 import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 
-
 export const googleAuth = async (req,res) => {
-    try {
-        const {name , email} = req.body
-        let user = await User.findOne({email})
-        if(!user){
-            user = await User.create({
-                name , 
-                email
-            })
-        }
-        let token = await genToken(user._id)
-        res.cookie("token" , token , {
-            http:true,
-            secure:true,
-            sameSite:"none",
-            maxAge:7 * 24 * 60 * 60 * 1000
-        })
+  try {
+    const {name , email} = req.body
 
-        return res.status(200).json(user)
+    let user = await User.findOne({email})
 
-
-
-    } catch (error) {
-        return res.status(500).json({message:`Google auth error ${error}`})
+    if(!user){
+      user = await User.create({
+        name,
+        email
+      })
     }
-    
+
+    let token = await genToken(user._id)
+
+    res.cookie("token", token, {
+      httpOnly: true,  // ← fixed typo (was http:true)
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+
+    return res.status(200).json(user)
+
+  } catch (error) {
+    return res.status(500).json({message:`Google auth error ${error}`})
+  }
 }
 
 export const logOut = async (req,res) => {
-    try {
-        await res.clearCookie("token")
-        return res.status(200).json({message:"LogOut Successfully"})
-    } catch (error) {
-         return res.status(500).json({message:`Logout error ${error}`})
-    }
-    
+  try {
+    await res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
+    })
+    return res.status(200).json({message:"LogOut Successfully"})
+  } catch (error) {
+    return res.status(500).json({message:`Logout error ${error}`})
+  }
 }
